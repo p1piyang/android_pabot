865814b56cf3d9541a16c92f2474829ec0d29a6f
p1piyang <m17550953521@163.com>
2020-08-21 18:08:16 +0800

    button完成

---
+++ ui_demo1/app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1 = findViewById(R.id.bt1);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
